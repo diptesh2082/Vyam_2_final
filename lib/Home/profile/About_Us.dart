2b7717b06e42e7d4cb4e2639e1de27e120440589
sarvagya <saxenasarvagya11@gmail.com>
2022-04-19 00:03:38 +0530

    profile changes

--- lib/Home/profile/About_Us.dart
+++ lib/Home/profile/About_Us.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AboutUs extends StatefulWidget {
   const AboutUs({Key? key}) : super(key: key);
@@ -8,26 +9,56 @@ class AboutUs extends StatefulWidget {
 }
 
 class _AboutUsState extends State<AboutUs> {
+  String text =
+      'Praesent sed tellus arcu. Donec ante nunc, semper sed eros a, condimentum hendrerit sapien. Vestibulum volutpat orci eu placerat malesuada. Phasellus non faucibus odio. Interdum et malesuada fames ac ante ipsum primis in faucibus. In suscipit neque in enim laoreet tincidunt. Sed auctor neque eget est accumsan';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("About Us"),
-        centerTitle: true,
-        foregroundColor: Color(0xff3A3A3A),
-        backgroundColor: Color(0xffE5E5E5),
-        elevation: 0,
-        automaticallyImplyLeading: true,
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back_ios),
-          onPressed: () => Navigator.pop(context, false),
-          color: Color(0xff3A3A3A),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("About Us"),
+          centerTitle: true,
+          foregroundColor: Color(0xff3A3A3A),
+          backgroundColor: Color(0xffE5E5E5),
+          elevation: 0,
+          automaticallyImplyLeading: true,
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back_ios),
+            onPressed: () => Navigator.pop(context, false),
+            color: Color(0xff3A3A3A),
+          ),
         ),
-      ),
-      body: Container(
-        color: Color(0xffE5E5E5),
-        child: Text(
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sed justo volutpat, hendrerit ante sagittis, rhoncus leo. Nunc eu libero porta, tincidunt elit non, ornare sapien. Etiam at nulla quis nulla viverra cursus. Praesent sed tellus arcu. Donec ante nunc, semper sed eros a, condimentum hendrerit sapien. Vestibulum volutpat orci eu placerat malesuada. Phasellus non faucibus odio. Interdum et malesuada fames ac ante ipsum primis in faucibus. In suscipit neque in enim laoreet tincidunt. Sed auctor neque eget est accumsan consectetur. Praesent sodales libero quis dui euismod facilisis. Nulla in eleifend ipsum. Curabitur quis mi sed quam accumsan cursus. Nulla maximus non ligula in ullamcorper. In euismod cursus purus at porttitor. In molestie ornare eros, scelerisque dignissim ex suscipit Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam sed justo volutpat, hendrerit ante sagittis, rhoncus leo. Nunc eu libero porta, tincidunt elit non, ornare sapien. Etiam at nulla quis nulla viverra cursus. Praesent sed tellus arcu. Donec ante nunc, semper sed eros a, condimentum hendrerit sapien. Vestibulum volutpat orci eu placerat malesuada. Phasellus non faucibus odio. Interdum et malesuada fames ac ante ipsum primis in faucibus. In suscipit neque in enim laoreet tincidunt. Sed auctor neque eget est accumsan consectetur. Praesent sodales libero quis dui euismod facilisis. Nulla in eleifend ipsum. Curabitur quis mi sed quam accumsan cursus. Nulla maximus non ligula in ullamcorper. In euismod cursus purus at porttitor. In molestie ornare eros, scelerisque dignissim ex suscipit eu sakjgdak kdajf kjdsg kljs dlkjsdlkgj isej lksdj klsgjd klsgjd lksjd gklsjd gisdj kjdsglk jgds;kj oisdgj skdgj skdgj ks;j lskdjg klsdjg lkjsdk;g jlskgj lsjksdj lsdjljlk sdgjklsdjg ksdj gkjsdkg jlskd gjk;sdj glksdjg lksjdg k;jdsg k;jdsg k;jsdg kjsd glkjsd;l gj;ksd gjksdjg ;ksdjg k;sdj jksfh djkh sdjk shdkjdsh fkjhds kjfhd sjkhjkds hjkd sfhjkhds jkhds jkhdsjk fhjskdhf ksdlkjlk sdfjlksdj flkdsj flkjdslkf  ;jsdfkljdsklfj klsdj fklsdj fkljsdifj klsdjf klsdjf lkdsfj ksd fmksdfm klsdm fklsdmf klsdm fimkl dmsfkmsdf klsdklfm klsdfm lkdsm fkdsm fklsdm fkmsdklf mkldsmf mds fkmdsklfm kdsm fkldsmf kldsmkl dsfmkldsm fkldms fklmds lfkmdskl mf kjldsf kljsd klfjlksd flkjds fkljsd fkljsdklj dlkfsjlkds jfdsj iisdfj klsdjf idsf jdskljf ilds f sdkjf klsjd flkjs dlksj dlkjsd lkfj slkjdsf  jksh dfjhds kkjh sdflhsd fjhs djkds hfkjd s jjhdsfjkh kjhsdjf khjksd hfjkshd fjkhsdjkhdskjfh jkhsdjk f"),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(40.0),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 20.0),
+                  child: Text(
+                    text,
+                    style: GoogleFonts.poppins(
+                      fontSize: 13,
+                      fontWeight: FontWeight.w400,
+                      textStyle: TextStyle(color: Colors.amber),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 20.0),
+                  child: Text(
+                    text,
+                    style: GoogleFonts.poppins(
+                      fontSize: 13,
+                      fontWeight: FontWeight.w400,
+                      textStyle: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
--- lib/Home/profile/Contact_Us.dart
+++ lib/Home/profile/Contact_Us.dart
@@ -13,83 +13,85 @@ class ContactUs extends StatefulWidget {
 class _ContactUsState extends State<ContactUs> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text("Contact Us"),
-          centerTitle: true,
-          foregroundColor: Color(0xff3A3A3A),
-          backgroundColor: Color(0xffE5E5E5),
-          elevation: 0,
-          automaticallyImplyLeading: true,
-          leading: IconButton(
-            icon: const Icon(Icons.arrow_back_ios),
-            onPressed: () => Navigator.pop(context, false),
-            color: Color(0xff3A3A3A),
-          ),
-        ),
-        body: Column(
-          children: [
-            ListTile(
-              leading: Icon(
-                FontAwesomeIcons.envelope,
-                color: Color(0xff292D32),
-                size: 20,
-              ),
-              title: Text(
-                'luffytaro@gmail.com',
-                style: GoogleFonts.poppins(
-                    fontSize: 14, fontWeight: FontWeight.w400),
-              ),
+    return SafeArea(
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text("Contact Us"),
+            centerTitle: true,
+            foregroundColor: Color(0xff3A3A3A),
+            backgroundColor: Color(0xffE5E5E5),
+            elevation: 0,
+            automaticallyImplyLeading: true,
+            leading: IconButton(
+              icon: const Icon(Icons.arrow_back_ios),
+              onPressed: () => Navigator.pop(context, false),
+              color: Color(0xff3A3A3A),
             ),
-            SizedBox(
-              child: Divider(
+          ),
+          body: Column(
+            children: [
+              ListTile(
+                leading: Icon(
+                  FontAwesomeIcons.envelope,
+                  color: Color(0xff292D32),
+                  size: 20,
+                ),
+                title: Text(
+                  'luffytaro@gmail.com',
+                  style: GoogleFonts.poppins(
+                      fontSize: 14, fontWeight: FontWeight.w400),
+                ),
+              ),
+              SizedBox(
+                child: Divider(
+                  color: Color(0xffE9E9E9),
+                  thickness: 1.5,
+                  height: 5,
+                ),
+              ),
+              ListTile(
+                leading: Icon(
+                  FontAwesomeIcons.instagram,
+                  color: Color(0xff292D32),
+                  size: 20,
+                ),
+                title: Text(
+                  '@vyam',
+                  style: GoogleFonts.poppins(
+                      fontSize: 14, fontWeight: FontWeight.w400),
+                ),
+              ),
+              Divider(
                 color: Color(0xffE9E9E9),
                 thickness: 1.5,
                 height: 5,
               ),
-            ),
-            ListTile(
-              leading: Icon(
-                FontAwesomeIcons.instagram,
-                color: Color(0xff292D32),
-                size: 20,
+              ListTile(
+                leading: Icon(
+                  FontAwesomeIcons.phone,
+                  color: Color(0xff292D32),
+                  size: 20,
+                ),
+                title: Text(
+                  '999893423',
+                  style: GoogleFonts.poppins(
+                      fontSize: 14, fontWeight: FontWeight.w400),
+                ),
               ),
-              title: Text(
-                '@vyam',
-                style: GoogleFonts.poppins(
-                    fontSize: 14, fontWeight: FontWeight.w400),
+              ListTile(
+                leading: Icon(
+                  FontAwesomeIcons.earthAmericas,
+                  color: Color(0xff292D32),
+                  size: 20,
+                ),
+                title: Text(
+                  'www.vyam.com',
+                  style: GoogleFonts.poppins(
+                      fontSize: 14, fontWeight: FontWeight.w400),
+                ),
               ),
-            ),
-            Divider(
-              color: Color(0xffE9E9E9),
-              thickness: 1.5,
-              height: 5,
-            ),
-            ListTile(
-              leading: Icon(
-                FontAwesomeIcons.phone,
-                color: Color(0xff292D32),
-                size: 20,
-              ),
-              title: Text(
-                '999893423',
-                style: GoogleFonts.poppins(
-                    fontSize: 14, fontWeight: FontWeight.w400),
-              ),
-            ),
-            ListTile(
-              leading: Icon(
-                FontAwesomeIcons.earthAmericas,
-                color: Color(0xff292D32),
-                size: 20,
-              ),
-              title: Text(
-                'www.vyam.com',
-                style: GoogleFonts.poppins(
-                    fontSize: 14, fontWeight: FontWeight.w400),
-              ),
-            ),
-          ],
-        ));
+            ],
+          )),
+    );
   }
 }
