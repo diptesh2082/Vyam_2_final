9d6612bff5b33c53d5e4ee7977baaee72e2e4264
Dewanshh <73938082+Dewanshh@users.noreply.github.com>
2022-05-25 14:01:41 +0530

    Added My Project

--- lib/Home/bookings/cancelpro.dart
+++ lib/Home/bookings/cancelpro.dart
@@ -1,22 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
 import 'package:vyam_2_final/OrderDetails/active_details.dart';
 
 class BooKingCancelation extends StatelessWidget {
   final doc;
-  const BooKingCancelation( {Key? key,required this.doc}) : super(key: key);
+  const BooKingCancelation({Key? key, required this.doc}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
-
-      onWillPop: ()async {
-        Get.off(()=>ActiveOrderDetails(),
-        arguments: {
-          "doc":doc
-        }
-        );
+      onWillPop: () async {
+        Get.off(() => ActiveOrderDetails(), arguments: {"doc": doc});
         return true;
       },
       child: Scaffold(
@@ -33,14 +29,10 @@ class BooKingCancelation extends StatelessWidget {
           elevation: 0.0,
         ),
         body: Center(
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
+          child: Column(mainAxisAlignment: MainAxisAlignment.center,
               // crossAxisAlignment: CrossAxisAlignment.center,
-              children:  [
-                Image.asset("assets/icons/x.gif",
-                height: 110,
-                  width: 110,
-                ),
+              children: [
+                Lottie.asset('assets/icons/asf.json', height: 100, width: 400),
                 SizedBox(height: 16.0),
                 Text(
                   "Booking Canceled!",
@@ -57,9 +49,7 @@ class BooKingCancelation extends StatelessWidget {
                     "Your Refund for pre-paid orders will  reflect within a maximum of 3-4 business days.",
                     textAlign: TextAlign.center,
                     style: GoogleFonts.poppins(
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.w500
-                    ),
+                        fontSize: 16.0, fontWeight: FontWeight.w500),
                   ),
                 ),
               ]),
